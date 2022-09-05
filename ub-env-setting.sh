67d461e1f675ef1337953812bf4d41d780570a05
ragrag6326 <ragrag6326@gmail.com>
2022-09-05 13:50:44 +0800

    Update ub-env-setting.sh

--- ub-env-setting.sh
+++ ub-env-setting.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # P1. sudo NOPASSWD  /etc/sudoers 
-cat <<EOF | sudo tee /etc/sudoers | sed "s|eth0|${KUBE_INTERFACE}|g" 
+cat <<EOF | sudo tee /etc/sudoers | sed "s|%sudo   ALL=(ALL:ALL) ALL|%sudo   ALL=(ALL:ALL) NOPASSWD:ALL|g" 
 EOF
