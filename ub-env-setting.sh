5144ace76928b7225d1c555ede25e9baa64259ed
ragrag6326 <ragrag6326@gmail.com>
2022-09-05 14:25:17 +0800

    Update ub-env-setting.sh

--- ub-env-setting.sh
+++ ub-env-setting.sh
@@ -2,4 +2,4 @@
 
 # P1. sudo NOPASSWD  /etc/sudoers 
 
-sudo cat test | sed '1 s/%sudo   ALL=(ALL:ALL) ALL/%sudo   ALL=(ALL:ALL) NOPASSWD:ALL/g' > /etc/sudoers
+sudo cat /etc/sudoers | sed '1 s/%sudo   ALL=(ALL:ALL) ALL/%sudo   ALL=(ALL:ALL) NOPASSWD:ALL/g' > /etc/sudoers
